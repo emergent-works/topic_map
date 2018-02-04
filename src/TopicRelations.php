e265085358f3bf7bbd6b71a52360eaff989fd31d
naomi <naomi@aktivix.org>
2018-02-04 16:19:03 +0000

    Changed machine names from just field_parents etc to field_topicmap_* for less likelehood of clashes with existing field names.

--- src/TopicRelations.php
+++ src/TopicRelations.php
@@ -5,9 +5,9 @@ namespace Drupal\topic_map;
 class TopicRelations {
 
   private $topic_relations = array(
-    array("base"=> "field_neighbours", "opposite"=>"field_neighbours"),
-    array("base"=> "field_parents", "opposite"=>"field_children"),
-    array("base"=> "field_children", "opposite"=>"field_parents"),
+    array("base"=> "field_topicmap_neighbours", "opposite"=>"field_topicmap_neighbours"),
+    array("base"=> "field_topicmap_parents", "opposite"=>"field_topicmap_children"),
+    array("base"=> "field_topicmap_children", "opposite"=>"field_topicmap_parents"),
   );
 
   public function onInsert($term) {
@@ -35,9 +35,9 @@ class TopicRelations {
     }
   }
 
-  private function getTargetIds($base_term, $field_name) {
+  private function getTargetIds($base_term, $field_topicmap_name) {
     $target_ids = array();
-    foreach($base_term->$field_name as $item) {
+    foreach($base_term->$field_topicmap_name as $item) {
       // sometimes entity is null
       if ($item->entity) {
         $target_ids[] = $item->entity->id();
@@ -46,24 +46,24 @@ class TopicRelations {
     return $target_ids;
   }
 
-  private function addRelationships($base_ids, $field_name, $target_id) {
+  private function addRelationships($base_ids, $field_topicmap_name, $target_id) {
     foreach($base_ids as $base_id) {
       $base_term = \Drupal\taxonomy\Entity\Term::load($base_id);
       // check that the relationship does not exist already. This stops recursion
       // and goes some way towards dealing with duplicates (since Drupal doesn't check).
-      if (!in_array($target_id, $this->getTargetIds($base_term, $field_name))) {
-        $base_term->$field_name->appendItem($target_id);  
+      if (!in_array($target_id, $this->getTargetIds($base_term, $field_topicmap_name))) {
+        $base_term->$field_topicmap_name->appendItem($target_id);  
         $base_term->save();
       }
     }
   }
 
-  private function removeRelationships($base_ids, $field_name, $target_id) {
+  private function removeRelationships($base_ids, $field_topicmap_name, $target_id) {
     foreach($base_ids as $base_id) {
       $base_term = \Drupal\taxonomy\Entity\Term::load($base_id);
-      foreach($base_term->$field_name->getValue() as $delta=>$value) {
+      foreach($base_term->$field_topicmap_name->getValue() as $delta=>$value) {
         if ($value['target_id'] == $target_id) {
-          $base_term->$field_name->removeItem($delta);
+          $base_term->$field_topicmap_name->removeItem($delta);
           $base_term->save();
           break;
         }
