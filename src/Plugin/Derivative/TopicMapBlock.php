c751cb8a787be33689b1fdfd8aa4c24014e0a457
naomi <naomi@aktivix.org>
2018-02-05 20:44:32 +0000

    Changed field names from field_topicmap_* to field_* for emergent works.

--- src/Plugin/Derivative/TopicMapBlock.php
+++ src/Plugin/Derivative/TopicMapBlock.php
@@ -16,13 +16,13 @@ class TopicMapBlock extends DeriverBase implements DeriverInterface {
   public function getDerivativeDefinitions($base_plugin_definition) {
     $vocabularies = \Drupal\taxonomy\Entity\Vocabulary::loadMultiple();
     foreach($vocabularies as $id => $vocab) {
-      $field_topicmap_definitions = \Drupal::entityManager()->getFieldDefinitions('taxonomy_term', $id);
-      $field_topicmap_names = array();
-      foreach($field_topicmap_definitions as $def) {
-        $field_topicmap_names[] = $def->getName();
+      $field_definitions = \Drupal::entityManager()->getFieldDefinitions('taxonomy_term', $id);
+      $field_names = array();
+      foreach($field_definitions as $def) {
+        $field_names[] = $def->getName();
       }   
       // If these fields are present on the vocabulary definition then it is a topic map vocabulary.
-      if (in_array('field_topicmap_children', $field_topicmap_names) && in_array('field_topicmap_parents', $field_topicmap_names) && in_array('field_topicmap_neighbours', $field_topicmap_names)) {
+      if (in_array('field_children', $field_names) && in_array('field_parents', $field_names) && in_array('field_neighbours', $field_names)) {
         $this->derivatives[$id] = $base_plugin_definition;
         $this->derivatives[$id]['admin_label'] = "Topic Map: $id";
       }   
