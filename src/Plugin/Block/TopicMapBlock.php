6266c552a050bcef325e8aa7752313324a9786eb
Naomi <naomirosenberguk@gmail.com>
2019-09-25 20:30:10 +0100

    Fixed links query to only include topics that are in the map!

--- src/Plugin/Block/TopicMapBlock.php
+++ src/Plugin/Block/TopicMapBlock.php
@@ -25,12 +25,19 @@ class TopicMapBlock extends BlockBase {
     $query->join("taxonomy_term__field_topics", "f", "f.field_topics_target_id = t.tid");    
     $query->condition("f.entity_id", $block_id);
 
-    $nodes = $query->execute()->fetchAll();
+    $topics = $query->execute()->fetchAll();
+
+    // Get a list of topic ids to plug into the query to get the links between them
+    foreach($topics as $topic) {
+      $tids[] = $topic->id;
+    }
+    $tids = join($tids, ',');
+
     // The links are parent-to-child and neighbour-to-neighbour. 
     // In the table, if a and b are neighbours there will be two rows - one in each direction. So we filter on source > target (as they cannot be the same node; this is ensured by the topic relations code.
-    $sql = "select concat(p.entity_id,'p',field_topicmap_parents_target_id) AS id,p.entity_id AS source,field_topicmap_parents_target_id AS target,'parent' AS relation from taxonomy_term__field_topicmap_parents p join taxonomy_term__field_topics t on (p.entity_id = t.field_topics_target_id or p.field_topicmap_parents_target_id = t.field_topics_target_id) where t.entity_id =  '" . $block_id . "' union select concat(n.entity_id,'n', field_topicmap_neighbours_target_id) AS id, n.entity_id AS source, field_topicmap_neighbours_target_id AS target,'neighbour' AS relation from taxonomy_term__field_topicmap_neighbours n join taxonomy_term__field_topics t on (n.entity_id = t.field_topics_target_id or n.field_topicmap_neighbours_target_id = t.field_topics_target_id) where t.entity_id  = '" . $block_id . "' and n.entity_id > field_topicmap_neighbours_target_id";
+    $sql = "select concat(p.entity_id,'p',field_topicmap_parents_target_id) AS id,p.entity_id AS source,field_topicmap_parents_target_id AS target,'parent' AS relation from taxonomy_term__field_topicmap_parents p where p.entity_id in ($tids) and field_topicmap_parents_target_id in ($tids) union select concat(n.entity_id,'n', field_topicmap_neighbours_target_id) AS id, n.entity_id AS source, field_topicmap_neighbours_target_id AS target,'neighbour' AS relation from taxonomy_term__field_topicmap_neighbours n where n.entity_id in ($tids) and field_topicmap_neighbours_target_id in ($tids) and n.entity_id > field_topicmap_neighbours_target_id";
     $links = db_query($sql)->fetchAll();
-    $container_size = sqrt(sizeof($nodes)) * 170;
+    $container_size = sqrt(sizeof($topics)) * 170;
     $output =  array (
         '#type' => 'inline_template',
         '#template' => '<div id="legend" class="panel-default panel">
@@ -50,7 +57,7 @@ class TopicMapBlock extends BlockBase {
     $output[]['#attached']['html_head'][] = [[
       '#type'  => 'html_tag' ,
       '#tag'   => 'script',
-      '#value' => 'var nodes = ' . json_encode($nodes),
+      '#value' => 'var nodes = ' . json_encode($topics),
 
     ], 'the_nodes'];
     $output[]['#attached']['html_head'][] = [[
