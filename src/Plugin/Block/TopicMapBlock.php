e47881a5c663a1f8c804aa6e85c15ac59ddb5059
Autonomic <helo@autonomic.zone>
2021-12-03 08:37:08 +0000

    set a width that works with all topic maps

--- src/Plugin/Block/TopicMapBlock.php
+++ src/Plugin/Block/TopicMapBlock.php
@@ -46,7 +46,7 @@ class TopicMapBlock extends BlockBase {
     $sql = "select concat(p.entity_id,'p',field_topicmap_parents_target_id) AS id,p.entity_id AS source,field_topicmap_parents_target_id AS target,'parent' AS relation from taxonomy_term__field_topicmap_parents p where p.entity_id in ($tids) and field_topicmap_parents_target_id in ($tids) union select concat(n.entity_id,'n', field_topicmap_neighbours_target_id) AS id, n.entity_id AS source, field_topicmap_neighbours_target_id AS target,'neighbour' AS relation from taxonomy_term__field_topicmap_neighbours n where n.entity_id in ($tids) and field_topicmap_neighbours_target_id in ($tids) and n.entity_id > field_topicmap_neighbours_target_id";
     $links = \Drupal::database()->query($sql)->fetchAll();
     $container_height = sqrt(sizeof($topics)) * 170;
-    $container_width = sqrt(sizeof($topics)) * 400;
+    $container_width = sqrt(sizeof($topics)) * 230 - 5 * sizeof($topics);
     $output['#template'] = Term::load($block_id)->getDescription() . '<div id="legend" class="panel-default panel">
                           <div class="panel-heading">Visual representation of the topic space</div>
                           <p>Hover over a topic to see its relationships to other topics: </p>
