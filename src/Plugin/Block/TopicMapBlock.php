66a5323bd8f47c9baf18905f0590c65165900e3b
naomi <naomirosenberguk@gmail.com>
2026-03-22 14:22:21 +0000

    Refactor
    Made TopicMapBlock the parent class and created a child class IKMTopicMap with the code specific to that in it.

--- src/Plugin/Block/TopicMapBlock.php
+++ src/Plugin/Block/TopicMapBlock.php
@@ -5,33 +5,22 @@ namespace Drupal\topic_map\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\taxonomy\Entity\Term;
 
-/**
- * Provides a Custom block.
- *
- * @Block(
-     id = "topic_map_block",
-     admin_label =  "Topic Map Block",
- *   category = @Translation("Topic Map"),
- *   deriver = "Drupal\topic_map\Plugin\Derivative\TopicMapBlock"
- * )
- */
 
-class TopicMapBlock extends BlockBase {
+abstract class TopicMapBlock extends BlockBase {
+
+  /**
+   * Extra text, if any, to add in to the description at the top of the topic map
+   */
+  protected string $extraInformation = '';
+
+
+  abstract protected function getTopics(string $block_id) :array ;
+
 
   public function build() {
-    $output = ['#type' => 'inline_template'];
     $block_id = $this->getDerivativeId();
-    $query = \Drupal::database()->select('taxonomy_term_field_data', 't');
-    $query->addField('t', 'tid', 'id');
-    $query->addField('t', 'name');
-    $query->join("taxonomy_term__field_topics", "f", "f.field_topics_target_id = t.tid");    
-    $query->leftJoin("taxonomy_term__field_type_of_topic", "ty", "ty.entity_id = t.tid");        
-    $query->leftJoin("taxonomy_term__field_descendents", "td", "td.entity_id = t.tid");    
-    $query->addField('ty', 'field_type_of_topic_value');
-    $query->addField('td', 'field_descendents_value');
-    $query->condition("f.entity_id", $block_id);
-
-    $topics = $query->execute()->fetchAll();
+    $topics = $this->getTopics($block_id);
+    $output = ['#type' => 'inline_template'];
 
     if (empty($topics)) {
       $output['#template'] = '<h1>There are no topics in this topic map yet. Edit it to add some.</h1>';
@@ -72,14 +61,9 @@ class TopicMapBlock extends BlockBase {
                               <li class="parents">topics that contain it</li> 
                               <li class="children">topics that are parts of it</li>  
                               <li class="neighbours">related topics</li>
-                            </ul>
-                          <p>There are three types of topics: </p>
-                            <ul>
-                              <li><span class="Subject">Subjects</span></li> 
-                              <li><span class="Process">Processes</span></li>  
-                              <li><span class="Relationship">Relationships</span></li>
-                            </ul>
-                          <p>Click on a topic to see information about it.</p>
+                            </ul>'
+                          . $this->extraInformation .
+                          '<p>Click on a topic to see information about it.</p>
                         </div>
                         <svg id="map_container" width="' . $container_width . '" height="' . $container_height . '"></svg>';
     $output[]['#attached']['library'][] = 'topic_map/d3';
